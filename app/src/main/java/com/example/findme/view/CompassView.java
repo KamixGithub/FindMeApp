4ee885e28242a259f86fb1e6665b37123288b125
Kami <sameerrahmann1625@gmail.com>
2024-05-29 22:09:49 +0200

    added 2 fragments (friendlist view, location view)

---
+++ app/src/main/java/com/example/findme/view/CompassView.java
@@ -0,0 +1,31 @@
+package com.example.findme.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.findme.R;
+
+public class CompassView extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_compass_view, container, false);
+    }
+
+
+}
